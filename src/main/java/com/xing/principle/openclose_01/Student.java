a060074cc25960ccf4e2280252c4047e28c45dd9
WanZhenxing <13629664531@163.com>
2024-10-29 15:14:15 +0800

    second commit！

--- src/main/java/com/xing/principle/openclose_01/Student.java
+++ src/main/java/com/xing/principle/openclose_01/Student.java
@@ -3,15 +3,15 @@ package com.xing.principle.openclose_01;
 public class Student implements Human {
     @Override
     public void eat() {
-        System.out.println("学生吃饭");
+        System.out.println("学生在吃饭");
     }
 
     @Override
     public void sleep() {
-        System.out.println("学生睡觉");
+        System.out.println("学生在睡觉");
     }
 
     public void study() {
-        System.out.println("学生学习");
+        System.out.println("学生在学习");
     }
 }
---
+++ src/main/java/com/xing/principle/openclose_01/Test.java
@@ -0,0 +1,7 @@
+package com.xing.principle.openclose_01;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
