7d4f95091582ce31177f48c05a87cd823c6eb977
WanZhenxing <13629664531@163.com>
2024-10-30 10:16:24 +0800

    01开闭原则 √！

--- src/main/java/com/xing/principle/openclose_01/Test.java
+++ src/main/java/com/xing/principle/openclose_01/Test.java
@@ -12,5 +12,10 @@ public class Test {
         highSchoolStudent.sleep();
         highSchoolStudent.study();
         highSchoolStudent.doSomeThing();
+
+        /**
+         * 软件实体（如类、模块、函数等）应当对扩展开放，对修改关闭。这意味着在不影响现有代码的情况下，通过增加新的类来实现新功能或行为。
+         * 学生和高中生都依赖于人类接口，如果还有高中生、研究生等等都是通过增加新的类并继承人类接口来实现新功能或行为。
+         */
     }
 }
