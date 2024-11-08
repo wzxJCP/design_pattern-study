cf7ac8b2dbc89075a1ad69df12fe1b8dbe2b0f83
WanZhenxing <13629664531@163.com>
2024-11-08 17:33:19 +0800

    23 2.1.5 单例模式-懒汉式 √！

---
+++ src/main/java/com/xing/pattern_23/creational_5/singleton_5/lazy_1/LazySingleton.java
@@ -0,0 +1,44 @@
+package com.xing.pattern_23.creational_5.singleton_5.lazy_1;
+
+import java.io.Serializable;
+
+public class LazySingleton implements Serializable {
+    // 防止反射调用
+    private static LazySingleton instance = null;
+
+    /**
+     * 私有构造方法，防止外部直接实例化。
+     * 通过检查 instance 是否为 null 来防止反射调用。
+     */
+    // private LazySingleton(){
+    //     if(instance != null){
+    //         throw new RuntimeException("单例构造器禁止反射调用");
+    //     }
+    // }
+
+    /**
+     * 获取单例实例的方法。
+     * 使用 synchronized 关键字保证线程安全。
+     * 只有当 instance 为 null 时才创建新的实例。
+     *
+     * @return 单例实例
+     * @author xing
+     */
+
+    public static synchronized LazySingleton getInstance(){
+        if(instance == null){
+            instance = new LazySingleton();
+        }
+        return instance;
+    }
+
+    /**
+     * 实现 Serializable 接口，并重写 readResolve 方法。
+     * 这是为了防止反序列化时创建新的实例，确保单例的唯一性。
+     *
+     * @return 单例实例
+     */
+    private Object readResolve(){
+        return instance;
+    }
+}
---
+++ src/main/java/com/xing/pattern_23/creational_5/singleton_5/lazy_1/Test.java
@@ -0,0 +1,24 @@
+package com.xing.pattern_23.creational_5.singleton_5.lazy_1;
+
+public class Test {
+    public static void main(String[] args) {
+        // 创建三个线程，每个线程都运行 Run 类的实例
+        Thread thread = new Thread(new Run());
+        Thread thread1 = new Thread(new Run());
+        Thread thread2 = new Thread(new Run());
+        // 启动这三个线程
+        thread.start();
+        thread1.start();
+        thread2.start();
+
+        // 打印当前主线程的信息
+        System.out.println("当前程序结束了......");
+    }
+}
+
+/**
+ * 当前程序结束了......
+ * Thread-0 com.xing.pattern_23.creational_5.singleton_5.doublechecked_2.DoubleCheckSingleton@5fccc883
+ * Thread-2 com.xing.pattern_23.creational_5.singleton_5.doublechecked_2.DoubleCheckSingleton@5fccc883
+ * Thread-1 com.xing.pattern_23.creational_5.singleton_5.doublechecked_2.DoubleCheckSingleton@5fccc883
+ */
