1e340d40261d600ca097f65844e254086f27afc6
Wan Zhenxing <13629664531@163.com>
2026-05-13 16:30:05 +0800

    b03_behavioral_11 √！

---
+++ src/main/java/com/xing/b_pattern_23/b03_behavioral_11/strategy_3/MultiplyStrategy.java
@@ -0,0 +1,13 @@
+package com.xing.b_pattern_23.b03_behavioral_11.strategy_3;
+
+/**
+ * @project：design_pattern-study
+ * @since：2024/11/18 14:45
+ * @author：wzx
+ */
+public class MultiplyStrategy implements Strategy {
+    @Override
+    public int doOperation(int num1, int num2) {
+        return num1 * num2;
+    }
+}
